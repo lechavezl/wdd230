ec86a529cc5e0b8e808d6315e71365d55cb4cc48
lechavezl <luisernestochavezleal@gmail.com>
2023-01-09 19:53:40 -0400

    Added current year, last updated date, and color fonts.

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,8 @@
-currentDate = new Date();
+// Get and show the current year
+let currentDate = new Date();
+let year = currentDate.getFullYear();
+document.querySelector("#currentYear").innerHTML = year;
+
+// Show the last time when the document was modified
+let lastModif = document.lastModified;
+document.querySelector("#lastUpdated").innerHTML = `Last Updated: ${lastModif}`;
