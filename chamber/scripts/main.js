07c6859c425d4efe99dc9a0d87716101c0af0ac2
lechavezl <luisernestochavezleal@gmail.com>
2023-02-11 01:40:10 -0400

    Finished chamber home page continued

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -23,3 +23,48 @@ document.getElementById("currentYear").innerHTML = year;
 // Show the last time when the document was modified
 let lastModif = document.lastModified;
 document.querySelector("#lastUpdated").innerHTML = `Last Modification: ${lastModif}`;
+
+// Display a banner when is monday or tuesday
+let banner = "";
+let currentDay = now.getDay();
+const getBanner = document.querySelector("#banner");
+
+switch(currentDay) {
+    case 0:
+        banner = "";
+        break;
+    case 1:
+        banner = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+        getBanner.style.color = "#000000";
+        getBanner.style.backgroundColor = '#1FC1C3';
+        getBanner.style.textAlign = "center";
+        getBanner.style.border = "1px solid #1FC1C3"
+        getBanner.style.borderBottomLeftRadius = "10px"
+        getBanner.style.borderBottomRightRadius = "10px"
+        getBanner.style.padding = "5px"
+        break;
+    case 2:
+        banner = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+        getBanner.style.color = "#000000";
+        getBanner.style.backgroundColor = '#1FC1C3';
+        getBanner.style.textAlign = "center";
+        getBanner.style.border = "1px solid #1FC1C3"
+        getBanner.style.borderBottomLeftRadius = "10px"
+        getBanner.style.borderBottomRightRadius = "10px"
+        getBanner.style.padding = "5px"
+        break;
+    case 3:
+        banner = "";
+        break;
+    case 4:
+        banner = "";
+        break;
+    case 5:
+        banner = "";
+        break;
+    case 6:
+        banner = "";
+        break;
+}
+
+const showBanner = getBanner.innerHTML = banner;
