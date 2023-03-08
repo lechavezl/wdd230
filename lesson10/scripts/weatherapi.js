6b2a6f504ea6f79b22308de0ed490aed1d6ec8f6
lechavezl <luisernestochavezleal@gmail.com>
2023-03-07 20:35:06 -0400

    Finished Weather API practice

--- lesson10/scripts/weatherapi.js
+++ lesson10/scripts/weatherapi.js
@@ -4,7 +4,7 @@ const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
 // Create a variable to hold the URL
-const url = "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks,units=imperial&appid=e2e3fe7cb77abe3984e683b38063793b";
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=e2e3fe7cb77abe3984e683b38063793b";
 
 // Create an async function and fetch() to retrieve the data
 async function apiFetch() {
@@ -29,9 +29,17 @@ async function apiFetch() {
 
     const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
     const desc = weatherData.weather[0].description;
-
+    
+    // Make uppercase the first letter of each word in the description
+    // 1. Use the split method to create an array of each word (Separate each word)
+    // 2. Use the map method to iterate over each word in the array.
+    // 3. Use the chartAt method to take the firts letter of each word.
+    // 4. Uppercase the first letter of each word
+    // 5. Use the slice method to get the rest of the word.
+    // 6. Use the join method to join the array into a single string.
+    let descUppercase = desc.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
     weatherIcon.setAttribute('src', iconsrc);
-    weatherIcon.setAttribute('alt', desc);
-    captionDesc.textContent = desc;
+    weatherIcon.setAttribute('alt', descUppercase);
+    captionDesc.textContent = descUppercase;
 
   }
