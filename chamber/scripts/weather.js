6f9aad46f1ada361370d5bd03564cca7a6c761f4
lechavezl <luisernestochavezleal@gmail.com>
2023-03-09 01:20:19 -0400

    Finished aplying Weather API in the home page

---
+++ chamber/scripts/weather.js
@@ -0,0 +1,74 @@
+const currentTemperature = document.querySelector("#temperature");
+const weatherIcon = document.querySelector("#weather-icon");
+const weatherDesc = document.querySelector("#currentCondition");
+const wSpeed = document.querySelector("#windSpeed");
+
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Anaco&units=metric&appid=e2e3fe7cb77abe3984e683b38063793b";
+
+async function fetchData() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        // console.log(data);
+        showResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+
+fetchData();
+
+function showResults(weatherData) {
+    //Get the temp and show it.
+    const anacoTemp = weatherData.main.temp.toFixed(0);
+    currentTemperature.textContent = anacoTemp;
+
+    //Transforming the temp into a float number
+    const floatTemperature = parseFloat(anacoTemp);
+    
+    // Get the icon weather 
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+    
+    // Get the wind speed.
+    const windSp = weatherData.wind.speed;
+
+    // Make uppercase the first letter of each word in the description
+    // 1. Use the split method to create an array of each word (Separate each word)
+    // 2. Use the map method to iterate over each word in the array.
+    // 3. Use the chartAt method to take the firts letter of each word.
+    // 4. Uppercase the first letter of each word
+    // 5. Use the slice method to get the rest of the word.
+    // 6. Use the join method to join the array into a single string.
+    let descUppercase = desc.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', descUppercase);
+    weatherDesc.textContent = descUppercase;
+    wSpeed.textContent = windSp;
+
+    // Calculate the wind chill
+    // 1. Convert Celsius to Fahrenheit and kilometers to miles
+    const fahTemperature = (floatTemperature * 9/5) + 32;
+    
+    // 2. Convert kilometers to miles
+    const mileSpeed = windSp * 0.62137;
+
+    // A conditional to make sure the values meet the requirements
+    if (fahTemperature <= 50 && mileSpeed > 3.0) {
+      
+      // if True calculate the wind chill
+      let windChill = 35.74 + (0.6215 * fahTemperature) - (35.75 * (windSp ** 0.16)) + (0.4275 * (fahTemperature * (windSp ** 0.16)));
+
+      let showWindChill = windChill.toFixed(2);
+      document.querySelector("#windChillValue").innerHTML = showWindChill;
+    }
+
+    else {
+      showWindChill = "N/A";
+      document.querySelector("#windChillValue").innerHTML = showWindChill;
+    }
+}
--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,30 +1,34 @@
-// Get the values temeprature and wind speed
-let temperature = document.querySelector("#temperature").innerHTML;
-let windSpeed = document.querySelector("#windSpeed").innerHTML;
+// // Get the values temeprature and wind speed
+// let temperature = document.querySelector("#temperature").innerHTML;
+// let windSpeed = document.querySelector("#windSpeed").innerHTML;
+// // let temval = parseInt(temperature);
+// // console.log(`this is a ${typeof(temperature)}`)
+// // console.log(`this is a ${typeof(windSpeed)}`)
+// // console.log(temval);
 
-// Convert the values to numbers (float and integers)
-let temperatureValue = parseInt(temperature);
-let windSpeedValue = parseFloat(windSpeed);
+// // Convert the values to numbers (float and integers)
+// let temperatureValue = parseInt(temperature);
+// let windSpeedValue = parseFloat(windSpeed);
 
-// Convert Celsius to Fahrenheit and k/m to m/h
-const fahTemperature = (temperatureValue * 9/5) + 32;
-const mileSpeed = windSpeedValue * 0.62137;
+// // Convert Celsius to Fahrenheit and k/m to m/h
+// const fahTemperature = (temperatureValue * 9/5) + 32;
+// const mileSpeed = windSpeedValue * 0.62137;
 
-// Create a function that calculates the wind chill
-function calculateWindChill(temp, wspeed) {
-    // A conditional to make sure the values meet the requirements
-    if (temp <= 50 && mileSpeed > 3.0) {
+// // Create a function that calculates the wind chill
+// function calculateWindChill(temp, wspeed) {
+//     // A conditional to make sure the values meet the requirements
+//     if (temp <= 50 && mileSpeed > 3.0) {
 
-        // if True calculate the wind chill        
-        let windChill = 35.74 + (0.6215 * temp) - (35.75 * (wspeed ** 0.16)) + (0.4275 * (temp * (wspeed ** 0.16)))
+//         // if True calculate the wind chill        
+//         let windChill = 35.74 + (0.6215 * temp) - (35.75 * (wspeed ** 0.16)) + (0.4275 * (temp * (wspeed ** 0.16)))
 
-        let showWindChill = `${windChill.toFixed(2)}°F`
-        return showWindChill;
-    }
+//         let showWindChill = `${windChill.toFixed(2)}°F`
+//         return showWindChill;
+//     }
 
-    else {
-        return "N/A";
-    }
-}
+//     else {
+//         return "N/A";
+//     }
+// }
 
-document.querySelector("#windChillValue").innerHTML = calculateWindChill(fahTemperature, mileSpeed);
+// document.querySelector("#windChillValue").innerHTML = calculateWindChill(fahTemperature, mileSpeed);
