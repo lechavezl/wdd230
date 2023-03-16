843f38b57bcc5b0aa8e36cd92b41eb633dea4370
lechavezl <luisernestochavezleal@gmail.com>
2023-03-16 00:53:44 -0400

    Make the table appears with a button

--- chamber/scripts/form-date.js
+++ chamber/scripts/form-date.js
@@ -5,3 +5,20 @@ const dateString = currentDate.toLocaleString();
 console.log(dateString);
 // Hold the value in the imput element
 document.getElementById("hiddenDate").value = dateString;
+
+// Get the botton element
+const toggleBtn = document.querySelector("#toggle-btn");
+// Get the table element
+const floatingTable = document.querySelector("#floating-table");
+// Get the close button element
+const closeBtn = document.querySelector("#close-btn");
+
+toggleBtn.addEventListener("click", () => {
+  floatingTable.style.display = floatingTable.style.display === "none" ? "block" : "none";
+  closeBtn.style.display = closeBtn.style.display === "none" ? "block" : "none";
+});
+
+closeBtn.addEventListener("click", () => {
+  floatingTable.style.display = "none";
+  closeBtn.style.display = "none"
+});
